b4ff068cbcf2a94f8fe7305f5786d2a2f60841df
Max <maxime.delannoy@outlook.com>
2021-01-14 10:18:43 +0100

    navabr created

---
+++ components/Navbar.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+
+const links = [
+  {
+    title: "Home",
+    description: "welcome to the truth",
+    url: "/",
+  },
+
+  {
+    title: "Market",
+    description: "Essential of the believer",
+    url: "/Market",
+  },
+
+  {
+    title: "Enlightenment by the truth",
+    description: "Achieve enlightenment",
+    url: "/the_truth",
+  },
+];
+
+export default function Navbar({ title, description, url }) {
+  return (
+    <ul id="list" className={styles.list}>
+      {links.map((link) => (
+        <li key={link.title}>
+          <Link href={link.url}>
+            <a className={styles.link} title={link.title}>
+              {link.description}
+            </a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
