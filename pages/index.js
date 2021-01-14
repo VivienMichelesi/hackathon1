d9cb09cb957adf5cd8c1b8d6c1c3fef9127de606
Emilie <martelemilie@hotmail.fr>
2021-01-14 16:19:52 +0100

    Merge main

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import Head from "next/head";
-<<<<<<< HEAD
 import JoinUS from "./JoinUs";
 import styles from "../styles/Home.module.css";
-=======
-import styles from "../styles/Home.module.css";
 import Link from "next/link";
->>>>>>> main
+
 
 export default function Home() {
   return (
@@ -15,28 +12,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-<<<<<<< HEAD
-      <JoinUS />
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-=======
       <main className={styles.main}>
         <p className="lienApropos">
           <Link href="/apropos/Apropos">
             <a>A propos</a>
           </Link>
         </p>
+        <p className="JoinUs">
+          <Link href="/JoinUs">
+            <a>Nous contacter</a>
+          </Link>
+        </p>
       </main>
->>>>>>> main
     </div>
   );
 }
