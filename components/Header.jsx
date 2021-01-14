8ebae6881b3cda4e44518f9177bbf78f1e2a3cc9
vivien leroux <vivienmichelesi@gmail.com>
2021-01-14 23:13:33 +0100

    Bannière ok

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,16 @@
+import styles from "../styles/Header.module.css";
+import Link from "next/link";
+
 export default function Header() {
-  return(
-    
+  return (
+    <header className={styles.headerWindows}>
+      <Link href="/">
+        <img
+          src="/images/banniere.png"
+          alt="banniere"
+          id="windowsBanniere"
+        ></img>
+      </Link>
+    </header>
   );
 }
