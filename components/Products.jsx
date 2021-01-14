3a679ac7945aab08983155ceabc8dfe4c408c857
Emilie <martelemilie@hotmail.fr>
2021-01-14 09:07:20 +0100

    Creation of the component responsible for displaying all products for sale

--- components/Products.jsx
+++ components/Products.jsx
@@ -1,3 +1,46 @@
+import Head from "next/head";
+import { getAllProducts } from "../products2";
+import styles from "../styles/Products.module.css";
+
 export default function Products() {
-  return <div className="Products"></div>;
+  const products = getAllProducts();
+
+  return (
+    <div className="Products">
+      <Head>
+        <title>Liste de nos produits</title>
+        <meta
+          name="viewport"
+          content="Cette page affiche l'ensemble des produits vendus sur notre site avec sa description, son prix et sa catégorie"
+          key="Liste de nos produits"
+        />
+      </Head>
+      <h1>Liste de nos produits</h1>
+      <div>
+        <button className={styles.btn} type="button">
+          GADGET
+        </button>
+        <button className={styles.btn} type="button">
+          APPLICATION
+        </button>
+      </div>
+      <div className={styles.cardGroup}>
+        {products.map((product) => (
+          <div className={styles.card} key={products.id}>
+            <img
+              className={styles.img}
+              src={product.image}
+              alt={product.name}
+            />
+            <div>
+              <h5 className={styles.cardTitle}>{product.name}</h5>
+              <span className="category">{product.category}</span>
+              <p className="description-text">{product.description}</p>
+              <span className="price">{product.price}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
